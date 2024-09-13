0b6fc2bbc14c20cfde2f17786109aee217f868e7
Donald FOTSO <donald.fotso@lmtgroup.com>
2024-09-13 17:02:38 +0200

    go

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   DEV_BASE_URL: 'http://localhost:8000/api/',
-  PROD_BASE_URL: 'http://cloud-gt1j76nl.hosteur.net:8080/insurance-management/api/'
+  PROD_BASE_URL: 'http://cloud-flran45h.hosteur.net:8080/patient-management/api/'
   };
   
